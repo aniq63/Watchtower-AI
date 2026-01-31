e0c1b12c66b815940d3cb197ece6274d06a09f44
Muhammad Aniq Ramzan <150286724+aniq63@users.noreply.github.com>
2026-01-31 16:52:13 +0500

    Delete demo.py

--- demo.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.routes import auth,get_api,projects
-
-app = FastAPI()
-
-app.include_router(auth.router)
-app.include_router(get_api.router)
-app.include_router(projects.router)
